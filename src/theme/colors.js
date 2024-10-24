a754382323b3621128dc357605c6c3b4d29748af
Leandromarinh <leandrinm14@gmail.com>
2024-10-23 22:22:06 -0300

    theme colors changed

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,8 +1,13 @@
+// const colors = {
+//   green: "#79A397",
+//   red: "#C94C4C",
+//   blue: "#A3A179",
+// };
+
 const colors = {
   green: "#79A397",
-  red: "#C94C4C",
-  blue: "#A3A179",
-  // blue: "#FAF6B9",
+  red: "#C58686",
+  blue: "#D9CBA1",
 };
 
 export default colors;
