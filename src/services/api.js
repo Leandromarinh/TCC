ce8c6add112add6af825c2c0a4c9290da542faa8
Leandromarinh <leandrinm14@gmail.com>
2024-10-23 22:00:20 -0300

    testing ci/cd workflow

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3200/" : null;
+  process.env.NODE_ENV === "development" ? "http://localhost:3200/" : "/api/";
 
 const api = axios.create({
   baseURL,
