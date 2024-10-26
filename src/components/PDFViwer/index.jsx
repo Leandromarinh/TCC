576184a8ed2c34da331d4f5f16bd320c1715fc9b
Leandromarinh <leandrinm14@gmail.com>
2024-10-25 23:33:50 -0300

    changed pdf to images

--- src/components/PDFViwer/index.jsx
+++ src/components/PDFViwer/index.jsx
@@ -4,6 +4,15 @@ import { useState } from "react";
 import { Container, ZoomControls, Img } from "./styles";
 
 import period1 from "../../assets/periods/period1.png";
+import period2 from "../../assets/periods/period2.png";
+import period3 from "../../assets/periods/period3.png";
+import period4 from "../../assets/periods/period4.png";
+import period5 from "../../assets/periods/period5.png";
+import period6 from "../../assets/periods/period6.png";
+import period7 from "../../assets/periods/period7.png";
+import period8 from "../../assets/periods/period8.png";
+import period9 from "../../assets/periods/period9.png";
+import period10 from "../../assets/periods/period10.png";
 
 export default function PDFViwer() {
   const [numPages, setNumPages] = useState();
@@ -15,7 +24,18 @@ export default function PDFViwer() {
     setNumPages(numPages);
   }
 
-  const periodList = [period1];
+  const periodList = [
+    period1,
+    period2,
+    period3,
+    period4,
+    period5,
+    period6,
+    period7,
+    period8,
+    period9,
+    period10,
+  ];
 
   const zoomIn = () => setScale(scale + 0.2);
   const zoomOut = () => setScale(scale > 0.5 ? scale - 0.2 : scale);
